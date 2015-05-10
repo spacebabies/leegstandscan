1aea3afd3332495e35be8098dbe3caa0620881ef
Joost Baaij <joost@spacebabies.nl>
2015-05-10 21:38:50 +0200

    added all questions

--- public/js/app.js
+++ public/js/app.js
@@ -39,12 +39,92 @@ $(document).foundation();
     {
       number: 1,
       name: "Het risico op hangjongeren",
-      points: {
-        high: 3,
-        medium: 0,
-        low: 0
-      },
       part: 0
+    },
+    {
+      number: 2,
+      name: "Het risico op vandalisme en vernieling, o.a. graffiti",
+      part: 0
+    },
+    {
+      number: 3,
+      name: "Het risico op inbraak/ diefstal, o.a. koper",
+      part: 0
+    },
+    {
+      number: 4,
+      name: "Het risico op brandstichting",
+      part: 0
+    },
+    {
+      number: 5,
+      name: "Het risico op kraak",
+      part: 0
+    },
+    {
+      number: 6,
+      name: "Het risico op hennepteelt",
+      part: 0
+    },
+    {
+      number: 7,
+      name: "Het risico op waterschade/ lekkage",
+      part: 1
+    },
+    {
+      number: 8,
+      name: "Het risico voor de brandveiligheid",
+      part: 1
+    },
+    {
+      number: 9,
+      name: "Het risico op technische storingen in installaties",
+      part: 1
+    },
+    {
+      number: 10,
+      name: "Het risico op verval door achterstallig onderhoud",
+      part: 1
+    },
+    {
+      number: 11,
+      name: "Het risico op verloedering van het buitenterrein",
+      part: 1
+    },
+    {
+      number: 12,
+      name: "Het risico op schade aan dak & gevel",
+      part: 1
+    },
+    {
+      number: 13,
+      name: "Het risico op onderverzekering",
+      part: 2
+    },
+    {
+      number: 14,
+      name: "Het risico op waardevermindering",
+      part: 2
+    },
+    {
+      number: 15,
+      name: "Het risico op energieverspilling",
+      part: 2
+    },
+    {
+      number: 16,
+      name: "Het risico op langere verkooptijd",
+      part: 2
+    },
+    {
+      number: 17,
+      name: "Het risico op verminderde leefbaarheid in de omgeving",
+      part: 2
+    },
+    {
+      number: 18,
+      name: "Het risico op imagoschade",
+      part: 2
     }
   ];
 
