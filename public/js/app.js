f845e3a3c8fcb3315aa7a785094abacb12f321ae
Joost Baaij <joost@spacebabies.nl>
2015-05-10 17:56:07 +0200

    render partials using angular-route

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,50 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+(function() {
+  var app = angular.module('leegstandscan', ['ngRoute']);
+
+  app.controller('WelcomeController', ['$scope',
+    function(){
+
+    }]);
+
+  app.controller('QuestionController', ['$scope', '$routeParams',
+    function($scope, $routeParams) {
+      $scope.questionId = $routeParams.questionId;
+    }]);
+
+  app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/vraag/:questionId', {
+        templateUrl: 'partials/question.html',
+        controller: 'QuestionController'
+      }).
+      otherwise({
+        templateUrl: 'partials/welcome.html',
+        controller: 'WelcomeController'
+      });
+  }]);
+
+  var parts = [
+    "Criminele risico's",
+    "Technische risico's",
+    "Economische risico's"
+  ]
+
+  var questions = [
+    {
+      number: 1,
+      name: "Het risico op hangjongeren",
+      points: {
+        high: 3,
+        medium: 0,
+        low: 0
+      },
+      part: 0
+    }
+  ];
+
+})();
